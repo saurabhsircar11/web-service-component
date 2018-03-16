58c30ce6919ac7e4b5351934ad36493cf020999b
sever <saurabh.sircar@theleanapps.com>
2018-03-16 12:03:17 +0530

    fixed query string body problem

--- webservicecomponent/webservice.js
+++ webservicecomponent/webservice.js
@@ -29,7 +29,7 @@ function optionMaker(jsonParams, method) {
         options['referrer'] = jsonParams.referrer.length > 0 ? jsonParams.referrer : 'client'
     }
     if (jsonParams.hasOwnProperty('queryStringBody')) {
-        options['body'] = jsonParams.queryStringBody.length > 0 ? jsonParams.queryStringBody : ''
+        options['body'] = jsonParams.queryStringBody
     }
     options['headers'] = jsonParams.headerJson
     return options
